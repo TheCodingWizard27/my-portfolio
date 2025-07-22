3847eaae22ae5b8d28a4755de4d681794077be18
Raunak Upreti <raunakupretics26@gmail.com>
2025-07-21 23:18:54 -0500

    Main Page Features added

---
+++ components/layouts/main.js
@@ -0,0 +1,47 @@
+import Head from 'next/head'
+import NavBar from '../navBar'
+import { Box, Container } from '@chakra-ui/react'
+import Footer from '../footer'
+import Image from 'next/image'
+
+const Main = ({ children, router }) => {
+  return (
+    <Box as="main" pb={8}>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+        <title>Raunak Upreti - Homepage</title>
+      </Head>
+
+      <NavBar path={router.asPath} />
+
+      <Container maxW="container.md" pt={32}>
+        <Box textAlign="center" mb={6}>
+          <Box
+            borderColor="whiteAlpha.800"
+            borderWidth={2}
+            borderStyle="solid"
+            w="100px"
+            h="100px"
+            display="inline-block"
+            borderRadius="full"
+            overflow="hidden"
+          >
+            <Image
+              src="/images/raunak.jpg"
+              alt="Profile image"
+              width="100"
+              height="100"
+            />
+          </Box>
+        </Box>
+
+        {children}
+
+        <Footer />
+      </Container>
+    </Box>
+  )
+}
+
+export default Main
