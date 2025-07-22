d243e1170c1c32ff91bb8b43101ad01ccf3ab9ea
Raunak Upreti <raunakupretics26@gmail.com>
2025-07-21 19:52:28 -0500

    All components required imported

---
+++ components/navBar.js
@@ -0,0 +1,19 @@
+import { forwardRef } from 'react'
+import NextLink from 'next/link'
+import {
+  Container,
+  Box,
+  Link,
+  Stack,
+  Heading,
+  Flex,
+  Menu,
+  MenuItem,
+  MenuList,
+  MenuButton,
+  IconButton,
+  useColorModeValue
+} from '@chakra-ui/react'
+import { HamburgerIcon } from '@chakra-ui/icons'
+import ThemeToggleButton from './theme-toggle-button'
+import { IoLogoGithub } from 'react-icons/io5'
