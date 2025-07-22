68d635a874bf91adb5885a2c29384f98221e691b
Raunak Upreti <raunakupretics26@gmail.com>
2025-07-22 17:25:07 -0500

    Moved the copyright footer a little down

--- components/footer.js
+++ components/footer.js
@@ -2,7 +2,7 @@ import { Box } from '@chakra-ui/react'
 
 const Footer = () => {
   return (
-    <Box align="center" opacity={0.4} fontSize="sm">
+    <Box align="center" opacity={0.4} fontSize="sm" mt={8}>
       &copy; {new Date().getFullYear()} Raunak Upreti. All Rights Reserved.
     </Box>
   )
