2e6e8343c13b3364f298c72d60f831636acf8d41
Raunak Upreti <raunakupretics26@gmail.com>
2025-07-21 23:25:06 -0500

    Projects Page added

---
+++ pages/projects.js
@@ -0,0 +1,19 @@
+import { Container, Heading, SimpleGrid } from '@chakra-ui/react'
+import Layout from '../components/layouts/article'
+
+const Projects = () => (
+  <Layout title="Projects">
+    <Container>
+      <Heading as="h3" fontSize={20} mb={4}>
+        Projects
+      </Heading>
+
+      <SimpleGrid columns={[1, 1, 2]} gap={6}>
+        {/* Your projects will go here */}
+      </SimpleGrid>
+    </Container>
+  </Layout>
+)
+
+export default Projects
+export { getServerSideProps } from '../components/chakra'
